f108944a10d6e4a11a511ca469d4b4df5f61453f
Erick Muñiz Morales <42720927+ErickMM98@users.noreply.github.com>
2020-03-27 01:08:04 -0600

    Add files via upload

---
+++ LGME_SMA_project/main.py
@@ -0,0 +1,18 @@
+"""
+FACULTAD DE CIENCIAS, UNAM
+SEMINARIO DE MATEMÁTICAS APLICADAS
+
+-MUÑIZ MORALES ERICK, @ErickMM98
+-GUZMÁN MORALES LÁZARO ABRAHAM
+
+lu: 27/04/20
+"""
+
+import networkx as nx
+import readata
+from random import random
+
+
+
+if __name__ == '__main__':
+    print(readata.read_all_lineup())
