d4dc7994392a4ea18e2a56e208e26f2b2d12c7ec
Erick Muñiz Morales <42720927+ErickMM98@users.noreply.github.com>
2020-05-19 20:19:33 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -2,9 +2,9 @@ from random import random as rd
 
 def moneda(p):
     if rd() < p:
-        return "Mandame tu video"
+        return "Hola"
     else:
-        return "Perdí, te mando mi foto :c"
+        return "Adiós"
 
 if __name__ == '__main__':
     print("")
